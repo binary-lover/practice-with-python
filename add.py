81cfe828bdcb1d43257646c1585ec15559fbb0f4
MS-Islam <95335243+MS-ISLAM@users.noreply.github.com>
2022-02-27 10:04:03 -0700

    Create add.py

---
+++ add.py
@@ -0,0 +1,15 @@
+#***********Very simple way without using function
+num1 = int(input("Enter 1st num"))
+num2 = int(input("Enter 2nd num"))
+sum = num1 + num2
+print("Sum is", sum)
+#or
+print("Sum is",num1+num2)
+
+
+#********By using function
+def add(a,b):
+	return a+b
+num1 = int(input("Enter 1st num"))
+num2 = int(input("Enter 2nd num"))
+print("Sum is",add(num1,num2)
