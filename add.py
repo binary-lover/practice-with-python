42964b8355fdadeaafbfa21dd51215a5f4284c60
Mozahidul Islam <95335243+binary-lover@users.noreply.github.com>
2023-02-11 09:35:57 +0000

    so nice to see this feature

--- add.py
+++ add.py
@@ -10,10 +10,6 @@ print("Sum is",num1+num2)
 #********By using function
 def add(a,b):
 	return a+b
-
 num1 = int(input("Enter 1st num"))
 num2 = int(input("Enter 2nd num"))
 print("Sum is",add(num1,num2))
-print("bye")
-#lucky asnri is the best
-#ans so on
