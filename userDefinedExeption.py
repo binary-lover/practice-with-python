7204bf00db95af2c6addbdfb948a8ff60fa07bbe
Mozahidul Islam <95335243+binary-lover@users.noreply.github.com>
2023-04-18 21:03:52 +0530

    Add files via upload

---
+++ userDefinedExeption.py
@@ -0,0 +1,31 @@
+# file = open("lucky.txt","w+")
+# file.write("buffer is the same thing that i use to play with \ni hope you will be file")
+
+
+# file.close()
+# file = open("lucky.txt","r")
+
+# data = file.read()
+# data = data.split()
+# print(data,type(data))
+
+print(bool(True)==1)
+
+# class insuficiantFund(Exception):
+#     pass
+
+# def withdraw():
+#     money = 10000
+#     withdraMoney = 9000
+#     try:
+#         balance = money - withdraMoney
+#         if (balance < 2000):
+#             raise insuficiantFund("amount insuficient")
+#     except insuficiantFund as e:
+#         print(e)
+        
+# withdraw()
+
+# # a = int(input("Enter value between 10 and 40"))
+# # if a>40 or a<10:
+# #     raise TypeError("Invalid error")
